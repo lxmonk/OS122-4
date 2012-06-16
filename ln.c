2e28609c1a9038809dbd041f9c3e13f18bb153be
tom sela <tsela85@gmail.com>
2012-06-16 22:51:22 +0300

    changed ln function

--- ln.c
+++ ln.c
@@ -5,11 +5,21 @@
 int
 main(int argc, char *argv[])
 {
-  if(argc != 3){
-    printf(2, "Usage: ln old new\n");
+    if((argc != 3) && (argc != 4)){
+        printf(2, "Usage: ln [-s] old new\n");
+        exit();
+    }
+    //A&T  old version - hard link
+    if (argc == 3) {
+        if(link(argv[1], argv[2]) < 0)
+            printf(2, "link %s %s: failed\n", argv[1], argv[2]);
+        exit();
+    }
+    //A&T soft link
+    if ((argv[1][0] == '-') && (argv[1][1] == 's'))
+        if (symlink(argv[2], argv[3]) < 0)
+            printf(2, "link -s %s %s: failed\n", argv[2], argv[3]);
+    else
+        printf(2, "Usage: ln [-s] old new\n"); //A&T not '-s'
     exit();
-  }
-  if(link(argv[1], argv[2]) < 0)
-    printf(2, "link %s %s: failed\n", argv[1], argv[2]);
-  exit();
 }
